6b1a2ab71f728d99158567ed17c0d0980277ce33
dariovidovic <malivida00@gmail.com>
2023-08-19 13:10:13 +0200

    commit

--- Bela blok/app/src/main/java/com/example/belablok/MainActivity.kt
+++ Bela blok/app/src/main/java/com/example/belablok/MainActivity.kt
@@ -1,21 +1,12 @@
-package com.example.belablok.feature_blok.presentation
+package com.example.belablok
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import com.example.belablok.feature_blok.data.data_source.BelaBlokDatabase
 import com.example.belablok.feature_blok.domain.model.Game
 import com.example.belablok.feature_blok.domain.model.History
 import com.example.belablok.feature_blok.domain.model.Record
-import com.example.belablok.ui.theme.BelaBlokTheme
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
